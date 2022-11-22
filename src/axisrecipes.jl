f6811f51eeb046b4ab48d52bf0bac75c6e5f6dbb
okatsn <okatsn@gmail.com>
2022-11-22 11:28:26 +0800

    The rest of makietools.jl from SWCForecast

---
+++ src/axisrecipes.jl
@@ -0,0 +1,64 @@
+
+
+"""
+`blankaxis!(fpos, xmin::T, ymin::T, xmax::T, ymax::T) where {T<:AbstractFloat}`
+creates an empty Makie Axis object at grid position `fpos`.
+
+# Example
+```julia
+f = Figure(;resolution=(1400,1000))
+gleft = f[1:3, 1] = GridLayout()
+gright = f[1:3, 2:3] = GridLayout()
+fpos = gright[0, :]
+blankaxis!(fpos, xmin, ymin, xmax, ymax)
+```
+"""
+function blankaxis!(fpos, xmin::T, ymin::T, xmax::T, ymax::T) where {T<:AbstractFloat}
+    ax = CairoMakie.Axis(fpos,
+        xticks = [xmin, xmax],
+        yticks = [ymin, ymax],
+        bottomspinevisible = false,
+        leftspinevisible = false,
+        xticklabelsvisible = false,
+        yticklabelsvisible = false,
+        xgridvisible = false,
+        ygridvisible = false,
+        topspinevisible = false,
+        rightspinevisible = false,
+    )
+return ax
+end
+
+
+
+
+"""
+`secondyaxis(f_grid; color = :black, ylabel = "y")` returns an `Axis` object of y axis to the right-hand side, with default settings for yyplot.
+
+# Example
+```julia
+f = Figure()
+ax_right = secondyaxis(f[1,1]; color=:red)
+
+
+```
+that you can do `lines!(ax_right, ...)`
+
+# Further reading
+- [Is it possible to add a secondary y axis?](https://github.com/JuliaPlots/Makie.jl/issues/816)
+"""
+function secondyaxis(f_grid; color = :black, ylabel = "y")
+        axright = CairoMakie.Axis(f_grid; ylabel = ylabel,
+        yaxisposition =:right,
+        xlabelvisible =false,
+        xticklabelsvisible =false,
+        xticksvisible=false,
+        xgridvisible = false,
+        ygridvisible = false,
+        ylabelcolor = color,
+        yticklabelcolor= color,
+        ytickcolor=color,
+        yminortickcolor = color
+    )
+    return axright
+end
---
+++ src/manipulatelimits.jl
@@ -0,0 +1,64 @@
+
+"""
+`expandylim!(ax, upper_expand)` expand the upper limit of y axis to a ratio of `upper_expand` of the original y range (`ymax - ymin`).
+
+# Example
+```julia
+expandylim!(ax, 0.25)
+```
+
+"""
+function expandylim!(ax, upper_expand)
+    xmin, ymin, xmax, ymax = getxylimits(ax)
+    diffy0 = ymax - ymin
+    newymax = ymin + diffy0*(1+upper_expand)
+    ylims!(ax, ymin, newymax)
+end
+
+"""
+`shrinkylim!(ax, ydatas::Vector{<:Number}; extent = 0.15)`
+fit y-limits to the input `ydatas` to with a certain `extent`.
+"""
+function shrinkylim!(ax, ydatas::Vector{<:Number}; extent = 0.15)
+    ymin, ymax = extrema(vcat(ydatas...))
+    # ymin = 0; ymax = 10
+    Δy = ymax - ymin
+    symin, symax = map((f, ym) -> f(ym, Δy*extent), [-,+], [ymin, ymax])
+    ylims!(ax, symin, symax)
+end
+
+
+"""
+# Example
+```julia
+xmin, ymin, xmax, ymax = getxylimits(ax)
+```
+"""
+function getxylimits(ax)
+    axlimtemp = ax.finallimits[]
+    ((xmin, ymin), (xmax,ymax)) = map(f -> f(axlimtemp), (minimum, maximum))
+    # see https://juliadatascience.io/glmakie
+    return xmin, ymin, xmax, ymax
+end
+
+"""
+`expandxylim!(ax, upper_expand_x, lower_expand_x, lower_expand_y, upper_expand_y)`
+TODO: merge with expandylim! in makietools.jl
+"""
+function expandxylim!(ax, upper_expand_x, lower_expand_x, lower_expand_y, upper_expand_y)
+    xmin, ymin, xmax, ymax = getxylimits(ax)
+    diffy0 = ymax - ymin
+    diffx0 = xmax - xmin
+    newymax = ymin + diffy0*(1+upper_expand_y)
+    newxmax = xmin + diffx0*(1+upper_expand_x)
+
+    newymmin = ymin - diffy0*lower_expand_y
+    newxmmin = xmin - diffx0*lower_expand_x
+    ylims!(ax, newymmin, newymax)
+    xlims!(ax, newxmmin, newxmax)
+end
+
+function expandxylim!(ax, f, upper_expand_x, lower_expand_x, lower_expand_y, upper_expand_y)
+    display(f); # this is required for expandxylim! to work properly
+    expandxylim!(ax, upper_expand_x, lower_expand_x, lower_expand_y, upper_expand_y)
+end
