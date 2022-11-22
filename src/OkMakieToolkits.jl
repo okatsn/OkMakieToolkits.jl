7f1ea101da5c4d931834594fc139bc8557f5899a
okatsn <okatsn@gmail.com>
2022-11-22 11:37:55 +0800

    Update module

--- src/OkMakieToolkits.jl
+++ src/OkMakieToolkits.jl
@@ -12,4 +12,20 @@ using CairoMakie
 include("dataoverview.jl")
 export dataoverview!
 
+
+
+include("axisrecipes.jl")
+export secondyaxis, blankaxis!
+
+include("manipulatelimits.jl")
+export getxylimits, expandylim!, shrinkylim!, expandxylim!
+
+
+using PlotUtils: optimize_ticks
+using Dates
+include("datetimeticks.jl")
+export datetimeticks!, datetime2epochs
+
+
+
 end
