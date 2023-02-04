bce2b615e35bc391f3f21d9e0415cde1bd843935
okatsn <okatsn@gmail.com>
2023-02-04 13:53:09 +0800

    Update CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,5 @@
 using Test
+# using TestItems
 using OkMakieToolkits
 
 function targetlist(targetexpr::Regex, dir2walk::AbstractString)
@@ -18,10 +19,31 @@ end
 
 (testfiles, allnames) = targetlist(r"^(?!runtests).*(\.jl)$", "./");
 # pwd() here should be "./test/"
-@testset "OkMakieToolkits" begin
+@testset "OkMakieToolkits.jl" begin
     for f in testfiles
         # write test in the file of the same name.
         # E.g., `test/mycode.jl` for testing `src/mycode.jl`.
         include(f)
     end
 end
+
+using Documenter
+@testset "DocTests" begin
+    DocMeta.setdocmeta!(OkMakieToolkits, :DocTestSetup, :(using OkMakieToolkits); recursive=true)
+    # using the package everywhere in the doc
+    # See also https://documenter.juliadocs.org/stable/man/doctests/#Setup-Code
+
+
+    doctest(OkMakieToolkits; manual = false) # this makes jldoctest also be tested in a local test
+end
+
+using CompatHelperLocal
+@testset "CompatHelperLocal" begin
+    @test true
+    TF = CompatHelperLocal.@check() # This raise warning only.
+end
+# @testitem "test vscode testitem" begin
+#     println("Only `@testitem` block will be detectable by vscode's test")
+#     @test true
+#     # See https://youtu.be/Okn_HKihWn8?t=1299
+# end
