63929dac6b84373f2c8dacbce0096b1e782331e1
okatsn <okatsn@gmail.com>
2023-08-23 16:50:22 +0800

    Refactor twinaxes

--- src/twinaxes.jl
+++ src/twinaxes.jl
@@ -1,3 +1,16 @@
+abstract type TwinAxis end
+
+struct LeftAxis <: TwinAxis
+    color
+    other
+end
+
+struct RightAxis <: TwinAxis
+    color
+    other
+end
+
+
 """
 `twinaxis(figpos::GridPosition; left_color=:black, right_color=:dodgerblue, left_axis=(;), right_axis=(;))` returns `(left_ax, right_ax)` for the left- and right- axis of the same plot on `figpos`.
 
@@ -21,26 +34,27 @@ RGBA{Float32}(0.0f0,0.0f0,1.0f0,1.0f0)
 
 Ref: https://beautiful.makie.org/dev/examples/generated/2d/lines/line_twin_axis/
 """
-function twinaxis(figpos::GridPosition; left_color=:black, right_color=:dodgerblue, left_axis=(;), right_axis=(;))
-    left_ax = Axis(figpos; rightspinevisible=false,
-        yticklabelcolor=left_color,
-        leftspinecolor=left_color,
-        ytickcolor=left_color,
-        left_axis...)
-    right_ax = Axis(figpos; yaxisposition=:right,
-        yticklabelcolor=right_color,
-        rightspinecolor=right_color,
-        ytickcolor=right_color,
-        ygridvisible=false,
-        right_axis...)
+function twinaxis(figpos::GridPosition; left_color=:black, left_axis=(;), right_color=:dodgerblue, right_axis=(;))
+    left_ax = _pinax(figpos, LeftAxis(left_color, left_axis))
+    right_ax = _pinax(figpos, RightAxis(right_color, right_axis))
     return (left_ax, right_ax)
 end
 
-# function _rightax(right_color, right_axis)
-#     right_ax = Axis(figpos; yaxisposition=:right,
-#         yticklabelcolor=right_color,
-#         rightspinecolor=right_color,
-#         ytickcolor=right_color,
-#         ygridvisible=false,
-#         right_axis...)
-# end
+function _pinax(figpos, ax::LeftAxis)
+    left_ax = Axis(figpos; rightspinevisible=false,
+        yticklabelcolor=ax.color,
+        leftspinecolor=ax.color,
+        ytickcolor=ax.color,
+        ax.other...)
+    return left_ax
+end
+
+function _pinax(figpos, ax::RightAxis)
+    right_ax = Axis(figpos; yaxisposition=:right,
+        yticklabelcolor=ax.color,
+        rightspinecolor=ax.color,
+        ytickcolor=ax.color,
+        ygridvisible=false,
+        ax.other...)
+    return right_ax
+end
