3978c5f084576ddd043e97d7d53c25bd3300131d
okatsn <okatsn@gmail.com>
2025-07-17 16:16:19 +0800

    Original script that color does not cycle

---
+++ recipe_loop_color_ZK.jl
@@ -0,0 +1,42 @@
+using CairoMakie
+
+@recipe(Plot_1d_Layer_Model, thicknesses, values) do scene
+    Attributes(
+        z0=0,
+    )
+end
+
+
+function Makie.plot!(plot::Plot_1d_Layer_Model)
+
+    thicknesses = plot[1][]
+    values = plot[2][]
+    # z0 = plot[:z0][]
+
+    x = vcat(values, values[end])
+    y = vcat(0, 0 .- cumsum(thicknesses), -1e10)
+
+    stairs!(plot, x, y)
+
+    return plot
+end
+
+
+fig = Figure(size=(900, 600))
+ax = Axis(fig[1, 1], xlabel="Resistivity (Ω·m)", ylabel="Elevation (m)")
+ylims!(ax, (-300, 50))
+
+true_model = [100, 40, 20]
+true_layers = [100.0, 100.0]
+plot_1d_layer_model!(ax, true_layers, true_model; label="Model 1")
+
+true_model = [80, 90, 50]
+true_layers = [50.0, 100.0]
+plot_1d_layer_model!(ax, true_layers, true_model; label="Model 2")
+
+true_model = [10, 70, 0]
+true_layers = [30.0, 80.0]
+plot_1d_layer_model!(ax, true_layers, true_model; z0=-10, color=:red, label="Model 3")
+axislegend(position=:rb)
+
+display(fig)
